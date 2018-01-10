49bfd8801149c191098522931517778e32adaf7b
yedashen <122649796@qq.com>
2018-01-10 10:09:22 +0800

    初始化项目

---
+++ app/src/main/java/tyj/com/thumbnaildemo/MainActivity.java
@@ -0,0 +1,16 @@
+package tyj.com.thumbnaildemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author ChenYe
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
